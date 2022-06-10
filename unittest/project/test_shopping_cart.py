fa3a6370119e84b369122dfb5ec599e7b3f169f4
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-09 23:07:37 -0500

    feat: :sparkles: add setUp and tearDown methods at method and class levels

--- unittest/project/test_shopping_cart.py
+++ unittest/project/test_shopping_cart.py
@@ -2,6 +2,27 @@ import unittest
 from product import Product
 
 class TestShoppingCart(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Metodo de clase"""
+        print('El metodo de clase setUpClass se ejecuta antes de todas las pruebas')
+    
+    @classmethod
+    def tearDownClass(cls):
+        """Metodo de clase"""
+        print('El metodo de clase tearDownClass se ejecuta despues de todas las pruebas')
+
+    def setUp(self) -> None:
+        print("El metodo setUp se ejecuta antes de cada una de las pruebas")
+        self.name = 'iPhone'
+        self.price = 500.00
+        self.smartphone = Product(self.name, self.price) # Se crea antes de c/u de las pruebas unitarias
+
+    def tearDown(self) -> None:
+        # print("El metodo tearDown se ejecuta despues de cada una de las pruebas")
+        pass
+
     def test_product_object(self):
         name = 'Manzana'
         price = 1.70
@@ -9,7 +30,13 @@ class TestShoppingCart(unittest.TestCase):
         product = Product(name, price)
 
         self.assertEqual(product.name, name)
-        self.assertEqual(product.price, 10.0, 'Lo sentimos el precio no es el mismo')
+        self.assertEqual(product.price, price, 'Lo sentimos el precio no es el mismo')
+
+    def test_product_name(self):
+        self.assertEqual(self.smartphone.name, self.name)
+    
+    def test_product_price(self):
+        self.assertEqual(self.smartphone.price, self.price)
 
 if __name__ == '__main__':
     unittest.main()
