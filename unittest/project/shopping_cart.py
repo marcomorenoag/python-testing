c8a1852893e420e82b54c86e99bcc6150c9033e6
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-10 14:24:47 -0500

    feat: :sparkles: test with assertTrue and assertFalse

--- unittest/project/shopping_cart.py
+++ unittest/project/shopping_cart.py
@@ -1,2 +1,15 @@
+from product import Product
+
+
 class ShoppingCart:
-    pass
+    def __init__(self) -> None:
+        self.__products: List[Product] = [] # Private field
+
+    def empty(self) -> bool:
+        return len(self.__products) == 0
+
+    def has_products(self) -> bool:
+        return not self.empty()
+
+    def add_product(self, product) -> None:
+        self.__products.append(product)
--- unittest/project/test_shopping_cart.py
+++ unittest/project/test_shopping_cart.py
@@ -1,23 +1,29 @@
 import unittest
 from product import Product
+from shopping_cart import ShoppingCart
 
 class TestShoppingCart(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         """Metodo de clase"""
-        print('El metodo de clase setUpClass se ejecuta antes de todas las pruebas')
+        # print('El metodo de clase setUpClass se ejecuta antes de todas las pruebas')
+        pass
     
     @classmethod
     def tearDownClass(cls):
         """Metodo de clase"""
-        print('El metodo de clase tearDownClass se ejecuta despues de todas las pruebas')
+        # print('El metodo de clase tearDownClass se ejecuta despues de todas las pruebas')
+        pass
 
     def setUp(self) -> None:
-        print("El metodo setUp se ejecuta antes de cada una de las pruebas")
+        # print("El metodo setUp se ejecuta antes de cada una de las pruebas")
         self.name = 'iPhone'
         self.price = 500.00
         self.smartphone = Product(self.name, self.price) # Se crea antes de c/u de las pruebas unitarias
+        self.shopping_cart_1 = ShoppingCart() 
+        self.shopping_cart_2 = ShoppingCart() 
+        self.shopping_cart_2.add_product(self.smartphone)
 
     def tearDown(self) -> None:
         # print("El metodo tearDown se ejecuta despues de cada una de las pruebas")
@@ -37,6 +43,13 @@ class TestShoppingCart(unittest.TestCase):
     
     def test_product_price(self):
         self.assertEqual(self.smartphone.price, self.price)
+
+    def test_shopping_cart_empty(self):
+        self.assertTrue(self.shopping_cart_1.empty(), 'Carrito de compras no está vacío')
+
+    def test_shopping_cart_has_products(self):
+        self.assertTrue(self.shopping_cart_2.has_products(), 'Carrito no tiene elementos')
+        self.assertFalse(self.shopping_cart_2.empty())
 
 if __name__ == '__main__':
     unittest.main()
