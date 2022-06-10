3d3dbac2b5e14f0e821394023b389c5fb1031a64
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-09 22:45:49 -0500

    feat: :sparkles: init with unittest

--- unittest/main.py
+++ unittest/main.py
@@ -9,23 +9,43 @@
 #     except AssertionError as error:
 #         print(error)
 
-def suma_numeros_positivos(n1: int, n2: int) -> int:
-    """Permite sumar dos numeros enteros positivos
+# def suma_numeros_positivos(n1: int, n2: int) -> int:
+#     """Permite sumar dos numeros enteros positivos
 
-    Args:
-        n1 (int)
-        n2 (int)
+#     Args:
+#         n1 (int)
+#         n2 (int)
 
-    Returns:
-        int
-    """
-    assert n1 > 0 and n2 > 0, 'Lo sentimos, solo es posible sumar numeros positivos'
-    return n1 + n2
+#     Returns:
+#         int
+#     """
+#     assert n1 > 0 and n2 > 0, 'Lo sentimos, solo es posible sumar numeros positivos'
+#     return n1 + n2
+
+
+# if __name__ == '__main__':
+#     try:
+#         resultado = suma_numeros_positivos(-10, 20)
+#         print(resultado)
+#     except AssertionError as error:
+#         print(error)
+
+import unittest
+
+class TestExample(unittest.TestCase):
+    def test_suma_numeros(self):
+        numero1 = 10
+        numero2 = 20
+
+        resultado = numero1 + numero2
+        # 30
+
+        self.assertEqual(resultado, 30)
+
+    def test_resta_numeros(self):
+        self.assertEqual(30 -20, 20)
 
 
 if __name__ == '__main__':
-    try:
-        resultado = suma_numeros_positivos(-10, 20)
-        print(resultado)
-    except AssertionError as error:
-        print(error)
+    # Ejecuta todos los métodos con el prefijo "test_" de la clase que herede TestCase
+    unittest.main()
