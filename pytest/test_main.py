44575c2d993a169c1b322f553ef577b0febbffe7
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-11 10:29:42 -0500

    feat: :sparkles: add setup and teardown at method and class level

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -1,16 +1,24 @@
-
 # Ejecutar todas las pruebas: pytest -v
 # Ejecutar pruebas de una clase puntual: pytest -v test_main.py::TestExample
 # Ejecutar prueba puntutal: pytest -v test_main.py::TestExample::test_resta_dos_numeros
 class TestExample():
+    @classmethod
+    def setup_class(cls):
+        print(">>> setup class se ejecuta antes de todas las pruebas")
+
+    @classmethod
+    def teardown_class(cls):
+        print(">>> teardown class se ejecuta después de todas las pruebas")
     
+    def setup_method(self):
+        self.numero_uno = 10
+        self.numero_dos = 20
+
+    def teardown_method(self):
+        pass
+
     def test_suma_dos_numeros(self):
-        assert 10 + 10 == 20, 'Lo sentimos, la suma no es correcta.'
+        assert self.numero_uno + self.numero_dos == 30, 'Lo sentimos, la suma no es correcta.'
 
     def test_resta_dos_numeros(self):
-        assert 10 - 10 == 0, 'Lo sentimos, la resta no es correcta.'
-
-class TestExample2():
-
-    def test_multiplicacion_dos_numeros(self):
-        assert 2 * 10 == 20, 'Lo sentimos, la multiplicacion no es correcta.'
+        assert self.numero_uno - self.numero_dos == -10, 'Lo sentimos, la resta no es correcta.'
