b44bdc944475da256380840d225fb2364d629f5d
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-11 18:08:31 -0500

    feat: :sparkles: integrate code coverage test module

--- pytest/app/__init__.py
+++ pytest/app/__init__.py
@@ -1,2 +1,5 @@
 from app.task import Task, DueDateError
 from app.task_manager import TaskManager
+
+# To run the coverage of code: coverage run -m pytest app/test/test_task.py -v
+# To see the result from .coverage file: coverage report -m
--- pytest/app/task.py
+++ pytest/app/task.py
@@ -6,7 +6,7 @@ class DueDateError(Exception):
 
 class Task():
     
-    def __init__(self, title, description, assigned_to, due_date):
+    def __init__(self, title, description, assigned_to, due_date, status='PENDING'):
         self.title = title
         self.description = description
         self.assigned_to = assigned_to
@@ -15,3 +15,10 @@ class Task():
             raise DueDateError('Lo sentimos, la fecha no es valida')
 
         self.due_date = due_date
+        self.status = status
+
+    def done(self):
+        self.status = 'DONE'
+
+    def undone(self):
+        self.status = 'PENDING'
