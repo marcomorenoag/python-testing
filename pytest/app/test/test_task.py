22d04e0881745c003781f21128271e061190e583
Marco Moreno <marcomoreno@wineadvisor.mx>
2022-06-11 10:56:39 -0500

    feat: :sparkles: add pytest marks to categorize tests

--- pytest/app/test/test_task.py
+++ pytest/app/test/test_task.py
@@ -2,11 +2,14 @@ import pytest
 from app import Task, DueDateError
 from datetime import datetime, timedelta
 
+# To run marked tests: pytest app/test/test_task.py -v -m due_date
 class TestTask():
     
+    @pytest.mark.news
     def test_task(self):
         assert True
 
+    @pytest.mark.news
     def test_new_task(self):
         due_date = datetime.now() + timedelta(days=1)
         task = Task('Title', 'Description', 'marco', due_date=due_date)
@@ -15,7 +18,15 @@ class TestTask():
         assert task.assigned_to == 'marco'
         assert task.due_date == due_date
 
+    @pytest.mark.due_date
+    @pytest.mark.errors
     def test_due_date_error(self):
         with pytest.raises(DueDateError):
             due_date = datetime.now() - timedelta(days=1)
             Task('Title1', 'Description1', 'marco1', due_date=due_date)
+
+    @pytest.mark.due_date
+    def test_due_date(self):
+        due_date = datetime.now() + timedelta(days=1)
+        task = Task('Title2', 'Description2', 'marco2', due_date=due_date)
+        assert task.due_date > datetime.now()
